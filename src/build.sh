d9b2c4c3bebc3cebbe91db7f375a23d36939f31f
Rodrigo Souza <rsouza01@gmail.com>
2019-08-19 21:35:50 +0200

    Build updated

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 rm ./*.o
-i686-elf-as boot.s -o boot.o
-i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+i686GCC=~/Projects/gcc-cc/bin
+
+#i686-elf-as boot.s -o boot.o
+nasm -felf32 boot.asm -o boot.o
+
+$i686GCC/i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+$i686GCC/i686-elf-gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+
+qemu-system-i386 -kernel myos.bin
